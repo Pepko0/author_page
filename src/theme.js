2327ff0bac18adf38c608e311c1f17abed6f40b9
Kacper Lewko <k.lewkosga@gmail.com>
2024-03-20 22:55:12 +0100

    changed name color in theme

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ export const theme = {
     color: {
         black: "#000000",
         white: "#FFFFFF",
-        gray: "#9DA0A4",
+        gray: "rgba(110, 126, 145, 1)",
         mainblue: "#0366D6",
         lightgray: "#f2f2f2",
         mercury: "#e2e2e2",
